d9fa613b28bf2d8fd6794c3c8384f5f6a0acb9a2
Samuel Paes <samuel.paes@propig.com.br>
2020-12-07 21:22:29 -0300

    Criação do algoritimo para uma estrutura de dados do tipo pilha

---
+++ ConsoleApp/Pilha.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class Pilha
+    {
+        Posicao primeiro;
+        public void Empilhar(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+        public object Desempilhar()
+        {
+            if(primeiro == null)
+            {
+                throw new InvalidCastException("A pilha está vazia!");
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;
+        }
+
+        class Posicao
+        {
+            public Posicao proximo;
+            public object item;
+            public Posicao(Posicao proximo, object item)
+            {
+                this.proximo = proximo;
+                this.item = item;
+            }
+        }
+    }
+}
