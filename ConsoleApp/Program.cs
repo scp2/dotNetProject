3f157e700508583c0ff466a8a3ce4ef29a3f31ff
Samuel Paes <samuel.paes@propig.com.br>
2020-12-13 11:28:54 -0300

    update repo

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,19 +1,35 @@
-﻿using System;
+﻿// using System;
 
-namespace ConsoleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var pilha = new Pilha();
-            pilha.Empilhar(1);
-            pilha.Empilhar(10);
-            pilha.Empilhar(100);
-            Console.WriteLine(pilha.Desempilhar());
-            Console.WriteLine(pilha.Desempilhar());
-            Console.WriteLine(pilha.Desempilhar());
-            Console.WriteLine(pilha.Desempilhar());
-        }
+// namespace ConsoleApp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var pilha = new Pilha();
+//             pilha.Empilhar(1);
+//             pilha.Empilhar(10);
+//             pilha.Empilhar(100);
+//             Console.WriteLine(pilha.Desempilhar());
+//             Console.WriteLine(pilha.Desempilhar());
+//             Console.WriteLine(pilha.Desempilhar());
+//             Console.WriteLine(pilha.Desempilhar());
+//         }
+//     }
+// }
+
+
+using System;
+
+class Desafio {
+    static void Main() {
+        int chico = 300 * Int32.Parse(Console.ReadLine());
+        int bento = 1500 * Int32.Parse(Console.ReadLine());
+        int bernardo = 600 * Int32.Parse(Console.ReadLine());
+        int marina = 1000 * Int32.Parse(Console.ReadLine());
+        int iara = 150 * Int32.Parse(Console.ReadLine());
+        int marlene = 225;
+        int total = chico + bento + bernardo + marina + iara + marlene; // Digite aqui o calculo total
+        Console.WriteLine(total.ToString());
     }
 }
