3572462869852bc8417bbd24d06c3924e734062f
Samuel Paes <samuel.paes@propig.com.br>
2020-12-13 12:21:41 -0300

    update repo

---
+++ CursoMVC/Models/Categorias.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace CursoMVC.Models
+{
+    public class Categorias
+    {
+        public int Id 
+        {
+            get;
+            set;
+        }
+        public string Descricao
+        {
+            get;
+            set;
+        }
+    }
+}
